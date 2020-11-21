63e2dc00cf291c04c3e25a439a5fd84d1e5d70c2
josedeblas <jose.deblas@atrapalo.com>
2020-11-21 15:35:16 +0100

    Blai v1

--- README.md
+++ README.md
@@ -1,3 +1,65 @@
 # blai
-Blai is a framework to build Neural Networks with python.
-Nowadays you can build the next algos: ReLU, sigmoid and tanh
+## Table of contents
+* [Introduction](#Introduction)
+* [Technologies](#technologies)
+* [Versions](#Versions)
+* [Setup](#setup)
+## Introduction
+Blai is a framework to build Neural Networks with python 3.
+
+Use case: Learning points distribution
+
+```
+py test_case.py
+Cost after iteration 0: 0.693113
+Cost after iteration 500: 0.297112
+Cost after iteration 1000: 0.282578
+Cost after iteration 1500: 0.274960
+Cost after iteration 2000: 0.269777
+Cost after iteration 2500: 0.265753
+Cost after iteration 3000: 0.262318
+Cost after iteration 3500: 0.259113
+Cost after iteration 4000: 0.241228
+Cost after iteration 4500: 0.230191
+Cost after iteration 5000: 0.226023
+Cost after iteration 5500: 0.223474
+Cost after iteration 6000: 0.221621
+Cost after iteration 6500: 0.220145
+Cost after iteration 7000: 0.218907
+Cost after iteration 7500: 0.217835
+Cost after iteration 8000: 0.216885
+Cost after iteration 8500: 0.216031
+Cost after iteration 9000: 0.215260
+Cost after iteration 9500: 0.218271
+Cost after iteration 9999: 0.217924
+Accuracy: 90%
+```
+
+![output test case](./images/output-test-case.png)
+## Technologies
+Blai needs the libraries listed bellow:
+* numpy
+* scikit-learn
+* matplotlib
+* 
+## Versions
+### blai v1
+Made to build simple Neural Networks.
+The activation functions developed are: ReLU, sigmoid and tanh
+It's possible to build as layers as you want with as neural units as you want.
+However, it's not developed any type of Regularization, so that, in really large neural networks could appear vanishing or exploding issues
+## Setup
+Just download the library. 
+Create your model with blai_nn_model function.
+Get your predictions with blai_predict function.
+Example with a hidden layer with 4 units, where the hidden layer activation function is tanh and the last layer activation function is sigmoid
+```
+parameters = blai_nn_model(X, Y, units_hidden_layers=[4], act_func="tanh", L_act_func="sigmoid", num_iterations=10000,
+                           print_cost=500)
+
+predictions = blai_predict(parameters, X, act_func="tanh", L_act_func="sigmoid")
+```
+
+
+
+
--- blai.py
+++ blai.py
@@ -1,7 +1,8 @@
 import numpy as np
 from blai_functions import *
 
-def blai_layer_sizes(X,Y,hidden_units):
+
+def blai_layer_sizes(X, Y, hidden_units):
     """
     Argument:
     X -- dataset of shape (number of features, number of examples)
@@ -12,10 +13,11 @@ def blai_layer_sizes(X,Y,hidden_units):
     layers -- List with the size of each layer, including input (layers[0] and output (layer[L])
     """
     layers = hidden_units
-    layers.insert(0,X.shape[0])
-    layers.insert(len(layers),Y.shape[0])
+    layers.insert(0, X.shape[0])
+    layers.insert(len(layers), Y.shape[0])
 
     return layers
+
 
 def blai_initialize_parameters(units_per_layer, blai_seed):
     """
@@ -32,18 +34,18 @@ def blai_initialize_parameters(units_per_layer, blai_seed):
                     WL -- weight matrix of shape (n_y, n_h_L)
                     bL -- bias vector of shape (n_y, 1)
     """
-    
+
     # we set up a seed so that your output matches ours although the initialization is random.
-    #np.random.seed(2) 
-    if(blai_seed > 0):
+    # np.random.seed(2)
+    if (blai_seed > 0):
         np.random.seed(blai_seed)
-    
+
     stabilizer = 0.01
     parameters = {}
     L = len(units_per_layer)
 
-    for l in range(1,L):
-        parameters["W" + str(l)] = np.random.randn(units_per_layer[l], units_per_layer[l-1])*stabilizer
+    for l in range(1, L):
+        parameters["W" + str(l)] = np.random.randn(units_per_layer[l], units_per_layer[l - 1]) * stabilizer
         parameters["b" + str(l)] = np.zeros((units_per_layer[l], 1))
 
     return parameters
@@ -65,25 +67,26 @@ def blai_forward_propagation(X, parameters, act_func, L_act_func):
     L = len(parameters) // 2
     cache = {}
 
-    for l in range(1,L):
-        if(l == 1):
+    for l in range(1, L):
+        if l == 1:
             A = X
         else:
-            A = cache["A"+str(l-1)]
+            A = cache["A" + str(l - 1)]
 
-        #linear func
-        cache["Z"+str(l)] = np.dot(parameters["W"+str(l)], A) + parameters["b"+str(l)]
+        # linear func
+        cache["Z" + str(l)] = np.dot(parameters["W" + str(l)], A) + parameters["b" + str(l)]
 
-        #activation func
-        cache["A"+str(l)] = blai_g(cache["Z"+str(l)], act_func)
+        # activation func
+        cache["A" + str(l)] = blai_g(cache["Z" + str(l)], act_func)
 
-    #Notice that the loop arrives until L-1, So we compute the last layer L with L_act_func
-    cache["Z"+str(L)] = np.dot(parameters["W"+str(L)],cache["A"+str(L-1)])+parameters["b"+str(L)]
-    cache["A"+str(L)] = blai_g(cache["Z"+str(L)], L_act_func)
+    # Notice that the loop arrives until L-1, So we compute the last layer L with L_act_func
+    cache["Z" + str(L)] = np.dot(parameters["W" + str(L)], cache["A" + str(L - 1)]) + parameters["b" + str(L)]
+    cache["A" + str(L)] = blai_g(cache["Z" + str(L)], L_act_func)
 
-    assert(cache["A"+str(L)].shape == (1, X.shape[1]))
-            
-    return cache["A"+str(L)], cache
+    assert (cache["A" + str(L)].shape == (1, X.shape[1]))
+
+    return cache["A" + str(L)], cache
+
 
 def blai_compute_cost(AL, Y):
     """
@@ -98,19 +101,20 @@ def blai_compute_cost(AL, Y):
     cost -- cross-entropy cost given equation for gradient descent and sigmoid
     
     """
-    
+
     # number of examples
     m = Y.shape[1]
 
     # Compute the cross-entropy cost
-    logprobs = np.multiply(Y,np.log(AL)) + np.multiply((1-Y),np.log(1-AL))
-    cost = - np.sum(logprobs)/m
-    
+    logprobs = np.multiply(Y, np.log(AL)) + np.multiply((1 - Y), np.log(1 - AL))
+    cost = - np.sum(logprobs) / m
+
     # makes sure cost is the dimension we expect. Turns [[17]] into 17 
     cost = float(np.squeeze(cost))
-    assert(isinstance(cost, float))
-    
+    assert (isinstance(cost, float))
+
     return cost
+
 
 def blai_backward_propagation(parameters, cache, X, Y, act_func):
     """
@@ -131,28 +135,29 @@ def blai_backward_propagation(parameters, cache, X, Y, act_func):
     L = len(parameters) // 2
     grads = {}
 
-    #The next reversed loop will start in L-1. So we calculate the gradients on L layer
-    grads["dZ"+str(L)] = cache["A"+str(L)] - Y
-    grads["dW"+str(L)] = np.dot(grads["dZ"+str(L)], cache["A"+str(L-1)].T) / m
-    grads["db"+str(L)] = np.sum(grads["dZ"+str(L)], axis = 1, keepdims=True) / m
+    # The next reversed loop will start in L-1. So we calculate the gradients on L layer
+    grads["dZ" + str(L)] = cache["A" + str(L)] - Y
+    grads["dW" + str(L)] = np.dot(grads["dZ" + str(L)], cache["A" + str(L - 1)].T) / m
+    grads["db" + str(L)] = np.sum(grads["dZ" + str(L)], axis=1, keepdims=True) / m
 
-    #And now the rest of
-    for l in reversed(range(1,L)):
+    # And now the rest of
+    for l in reversed(range(1, L)):
 
-        grads["dZ"+str(l)] = np.multiply(parameters["W"+str(l+1)].T, grads["dZ"+str(l+1)]) * blai_derivative_g(cache, l, act_func)
+        grads["dZ" + str(l)] = np.multiply(parameters["W" + str(l + 1)].T,
+                                           grads["dZ" + str(l + 1)]) * blai_derivative_g(cache, l, act_func)
 
-        if(l == 1):
+        if l == 1:
             A = X.T
         else:
-            A = cache["A"+str(l-1)].T
+            A = cache["A" + str(l - 1)].T
 
-        grads["dW"+str(l)] = np.dot(grads["dZ"+str(l)], A) / m
-        grads["db"+str(l)] = np.sum(grads["dZ"+str(l)], axis = 1, keepdims=True) / m
-        
+        grads["dW" + str(l)] = np.dot(grads["dZ" + str(l)], A) / m
+        grads["db" + str(l)] = np.sum(grads["dZ" + str(l)], axis=1, keepdims=True) / m
 
     return grads
 
-def blai_update_parameters(parameters, grads, learning_rate = 1.2):
+
+def blai_update_parameters(parameters, grads, learning_rate=1.2):
     """
     Arguments:
     parameters -- python dictionary containing our parameters W[i] and b[i]
@@ -164,13 +169,15 @@ def blai_update_parameters(parameters, grads, learning_rate = 1.2):
     """
     L = len(parameters) // 2
 
-    for l in range(1,L+1):
-        parameters["W"+str(l)] = parameters["W"+str(l)] - (learning_rate * grads["dW"+str(l)])
-        parameters["b"+str(l)] = parameters["b"+str(l)] - (learning_rate * grads["db"+str(l)])
+    for l in range(1, L + 1):
+        parameters["W" + str(l)] = parameters["W" + str(l)] - (learning_rate * grads["dW" + str(l)])
+        parameters["b" + str(l)] = parameters["b" + str(l)] - (learning_rate * grads["db" + str(l)])
 
     return parameters
 
-def blai_nn_model(X, Y, units_hidden_layers, act_func="relu", L_act_func="relu", learning_rate = 1.2, num_iterations = 1000, print_cost=0, blai_seed=0):
+
+def blai_nn_model(X, Y, units_hidden_layers, act_func="relu", L_act_func="relu", learning_rate=1.2, num_iterations=1000,
+                  print_cost=0, blai_seed=0):
     """
     Arguments:
     X -- dataset of shape (number of features, number of examples)
@@ -179,7 +186,7 @@ def blai_nn_model(X, Y, units_hidden_layers, act_func="relu", L_act_func="relu",
     act_func -- activation function for layers from l=1 to l=L-1. ReLU by default
     L_act_func -- activation function for the last layer L. ReLU by default
     learning_rate -- hyperparameter alpha to update parameters
-    num_iterations -- Number of iterations in gradient descent loop
+    num_iterations -- Number of iterations or epochs in gradient descent loop
     print_cost -- if print_cost > 0 the cost is printed every print_cost iterations
     blai_seed -- to set a seed along blai functions
 
@@ -187,33 +194,36 @@ def blai_nn_model(X, Y, units_hidden_layers, act_func="relu", L_act_func="relu",
     parameters -- parameters learnt by the model. They can then be used to predict.
     """
 
-    #np.random.seed(3)
-    if(blai_seed > 0):
+    # np.random.seed(3)
+    if (blai_seed > 0):
         np.random.seed(blai_seed)
 
     units_per_layer = blai_layer_sizes(X, Y, units_hidden_layers)
-    
+
     parameters = blai_initialize_parameters(units_per_layer, blai_seed)
 
     for i in range(0, num_iterations):
 
         # Forward propagation
         AL, cache = blai_forward_propagation(X, parameters, act_func, L_act_func)
-        
+
         # Cost function
         cost = blai_compute_cost(AL, Y)
- 
+
         # Backpropagation
         grads = blai_backward_propagation(parameters, cache, X, Y, act_func)
- 
+
         # Gradient descent parameter update
         parameters = blai_update_parameters(parameters, grads, learning_rate)
-        
+
         # Print the cost every i iterations
         if print_cost > 0 and i % print_cost == 0:
-            print ("Cost after iteration %i: %f" %(i, cost))
+            print("Cost after iteration %i: %f" % (i, cost))
+
+    print("Cost after iteration %i: %f" % (i, cost))
 
     return parameters
+
 
 def blai_predict(parameters, X, act_func="tanh", L_act_func="sigmoid"):
     """
@@ -226,11 +236,11 @@ def blai_predict(parameters, X, act_func="tanh", L_act_func="sigmoid"):
     Returns
     predictions -- vector of predictions of our model (red: 0 / blue: 1)
     """
-    
-    #binary
+
+    # binary
     # Computes probabilities using forward propagation, and classifies to 0/1 using 0.5 as the threshold.
     A2, cache = blai_forward_propagation(X, parameters=parameters, act_func=act_func, L_act_func=L_act_func)
 
     predictions = A2 > 0.5
-    
+
     return predictions
